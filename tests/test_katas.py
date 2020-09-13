9f2f9fdf514735562f411335d7f5f30fea3298e7
erica best <purple_lover_04@msn.com>
2020-09-13 16:36:25 -0400

    all test past

--- tests/test_katas.py
+++ tests/test_katas.py
@@ -23,7 +23,7 @@ class TestKatas(unittest.TestCase):
 
     def test_factorial(self):
         list_factorial = [
-            1,1, 2, 6, 24, 120, 720, 5040, 40320, 362880, 3628800,
+            1, 1, 2, 6, 24, 120, 720, 5040, 40320, 362880, 3628800,
             39916800, 479001600, 6227020800, 87178291200, 1307674368000]
         for i, value in enumerate(list_factorial):
             self.assertEqual(katas.factorial(i), value)
