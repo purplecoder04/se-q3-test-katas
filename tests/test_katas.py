47d9899b3bdc857b677054f51a95569fcd3307e1
erica best <purple_lover_04@msn.com>
2020-09-13 16:35:21 -0400

    all test past

--- tests/test_katas.py
+++ tests/test_katas.py
@@ -1,22 +1,43 @@
 import unittest
 import katas
+import random
 
 
 class TestKatas(unittest.TestCase):
     def test_add(self):
-        self.fail("TODO: Write add unit test")
+        number = random.randrange(-4, 25)
+        number2 = random.randrange(-10, 250)
+        total = number + number2
+        self.assertEqual(katas.add(number, number2), total)
 
     def test_multiply(self):
-        self.fail("TODO: Write multiply unit test")
+        number = random.randrange(-4, 25)
+        number2 = random.randrange(-10, 250)
+        total = number * number2
+        self.assertEqual(katas.multiply(number, number2), total)
 
     def test_power(self):
-        self.fail("TODO: Write power unit test")
+        self.assertEqual(katas.power(2, 2), 4)
+        self.assertRaises(ValueError, katas.power, 4, -5)
+        self.assertRaises(ValueError, katas.power, 5, 0.52)
 
     def test_factorial(self):
-        self.fail("TODO: Write factorial unit test")
+        list_factorial = [
+            1,1, 2, 6, 24, 120, 720, 5040, 40320, 362880, 3628800,
+            39916800, 479001600, 6227020800, 87178291200, 1307674368000]
+        for i, value in enumerate(list_factorial):
+            self.assertEqual(katas.factorial(i), value)
+            self.assertRaises(ValueError, katas.factorial, -5)
 
     def test_fibonacci(self):
-        self.fail("TODO: Write fibonacci unit test")
+        list_fibs = [
+            0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144,
+            233, 377, 610, 987, 1597, 2584, 4181, 6765,
+            10946, 17711, 28657, 46368, 75025, 121393, 196418, 317811,
+            514229, 832040]
+        for i, value in enumerate(list_fibs):
+            self.assertEqual(katas.fibonacci(i), value)
+            self.assertRaises(ValueError, katas.fibonacci, -5)
 
 
 if __name__ == '__main__':
